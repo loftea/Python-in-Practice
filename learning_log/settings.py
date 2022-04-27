94226cca2b79a00d75e3fdc3574828e50a371456
Eussikun <57883305+loftea@users.noreply.github.com>
2022-04-28 00:59:43 +0800

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,10 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-LOGIN_URL='users:login'
-
-import django_heroku
-django_heroku.settings(locals())
 
 # Application definition
 
@@ -132,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+
+django_heroku.settings(locals())
